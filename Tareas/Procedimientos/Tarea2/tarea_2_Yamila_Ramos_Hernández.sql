f575c84c634edf80d41f97a125a4664a5df47139
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-03-30 15:29:31 +0100

    Realización de la tarea2

--- Tareas/Procedimientos/Tarea2/tarea_2_Yamila_Ramos_Hernández.sql
+++ Tareas/Procedimientos/Tarea2/tarea_2_Yamila_Ramos_Hernández.sql
@@ -17,8 +17,37 @@ insert into empleados values (22555555, 'Susana', 'Garcia', 400.00, 2, 'Secretar
 insert into empleados values (22666666, 'Jose Maria', 'Morales', 400.00, 3, 'Secretaria');
 
 --- 3- Elimine el procedimiento llamado pa_empleados_sueldo si existe.
-drop procedure if exist pa_empleados_sueldo;
+drop procedure if exists pa_empleados_sueldo; 
 
 --- 4- Cree un procedimiento almacenado llamado pa_empleados_sueldo que seleccione los nombres.
 DELIMITER //
-create procedure pa_empleados_sueldo
+create procedure pa_empleados_sueldo()
+BEGIN
+    select nombre from empleados;
+END 
+//
+
+--- 5- Ejecute el procedimiento creado anteriormente.
+DELIMITER ;
+CALL pa_empleados_sueldo() //
+
+--- 6- Elimine el procedimiento llamado pa_empleados_hijos si existe.
+drop procedure if exists pa_empleados_hijos;
+
+--- 7- Cree un procedimiento almacenado llamado pa_empleados_hijos que seleccione los nombres apellidos y cantidad de hijos de los empleados con hijos.
+DELIMITER //
+CREATE PROCEDURE pa_empleados_hijos()
+BEGIN
+    select nombre, apellido, cantidad_hijos from empleados where cantidad_hijos > 0;
+END 
+//
+
+--- 8- Ejecute el procedimiento creado anteriormente.
+DELIMITER ;
+CALL pa_empleados_hijos();
+
+
+--- 9- Actualice la cantidad de hijos de algún empleado sin hijos y vuelva a ejecutar el procedimiento para verificar que ahora si aparece en la lista.
+update empleados set cantidad_hijos = 1 where cantidad_hijos = 0;
+
+call pa_empleados_hijos();
