7803a2b88a9d62d716960cce679fcccc5f39b299
Yaamiilaa <yamilaramoshdez@gmail.com>
2023-04-11 14:54:02 +0100

    Modificación de la tarea 3 de procedimientos

--- Tareas/Procedimientos/Tarea3/donacion.sql
+++ Tareas/Procedimientos/Tarea3/donacion.sql
@@ -8,28 +8,24 @@ create table donacion(
 );
 
 --- Realizamos 10 insert en la tabla.
-insert into donacion values (52, Si, H, ultima_donacion);
-insert into donacion values (84, No, M, ultima_donacion);
-insert into donacion values (76, Si, M, ultima_donacion);
-insert into donacion values (35, No, H, ultima_donacion);
-insert into donacion values (90, No, H, ultima_donacion);
-insert into donacion values (83, Si, M, ultima_donacion);
-insert into donacion values (30, No, M, ultima_donacion);
-insert into donacion values (93. No, H, ultima_donacion);
-insert into donacion values (65, Si, M, ultima_donacion);
-insert into donacion values (102, No, H, ultima_donacion);
+insert into donacion values (52, 'Si', 'H', ultima_donacion);
+insert into donacion values (84, 'No', 'M', ultima_donacion);
+insert into donacion values (76, 'Si', 'M', ultima_donacion);
+insert into donacion values (35, 'No', 'H', ultima_donacion);
+insert into donacion values (90, 'No', 'H', ultima_donacion);
+insert into donacion values (83, 'Si', 'M', ultima_donacion);
+insert into donacion values (30, 'No', 'M', ultima_donacion);
+insert into donacion values (93, 'No', 'H', ultima_donacion);
+insert into donacion values (65, 'Si', 'M', ultima_donacion);
+insert into donacion values (102, 'No', 'H', ultima_donacion);
 
 --- Procedimiento para saber si la persona es apta para donar sangre
 DELIMITER $$
 DROP PROCEDURE IF EXISTS apto_para_donar $$
 create procedure apto_para_donar()
 BEGIN 
-IF peso < 50
-then
-set admitido = 'No';
-ELSE 
-set admitido = 'Si';
-END 
-IF;
+    update donacion set admitido = 'Si' where peso > 50;
+    update donacion set admitido = 'No' where peso < 50;
 
 END $$
+
