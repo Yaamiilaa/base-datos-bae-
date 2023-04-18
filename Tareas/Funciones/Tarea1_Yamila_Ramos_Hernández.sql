d1cf8230656eb7571f2acd6545c4cd00175cefe5
Yaamiilaa <113527458+Yaamiilaa@users.noreply.github.com>
2023-04-18 15:06:24 +0100

    Modificación de la tarea 1 de funciones

--- Tareas/Funciones/Tarea1_Yamila_Ramos_Hernández.sql
+++ Tareas/Funciones/Tarea1_Yamila_Ramos_Hernández.sql
@@ -26,9 +26,9 @@ insert into salario values(10, 'María', 1000);
 --- 1. Función subsidio_transporte: El subsidio de transporte equivale al 7% al salario básico.
 DELIMITER //
 DROP FUNCTION IF EXISTS subsidio_transporte //
-CREATE FUNCTION subsidio_transporte(salario_base int) RETURNS int DETERMINISTIC
+CREATE FUNCTION subsidio_transporte(salario_base int) RETURNS float DETERMINISTIC
 BEGIN 
-    DECLARE calcular_subsidio int;
+    DECLARE calcular_subsidio float;
     set calcular_subsidio = salario_base * 0.07;
     RETURN calcular_subsidio;
 END 
@@ -37,9 +37,9 @@ END
 --- Otra manera de hacer el ejercicio 1.
 DELIMITER //
 DROP FUNCTION IF EXISTS subsidio_transporte //
-CREATE FUNCTION subsidio_transporte(id int) RETURNS int DETERMINISTIC
+CREATE FUNCTION subsidio_transporte(id int) RETURNS float DETERMINISTIC
 BEGIN 
-    declare calcular_subsidio int;
+    declare calcular_subsidio float;
     set calcular_subsidio = (select salario_base * 0.07 from salario where identificador = id);
     RETURN calcular_subsidio;
 END 
@@ -48,9 +48,9 @@ END
 --- 2. Función salud: La salud que corresponde al 4% al salario básico.
 DELIMITER //
 DROP FUNCTION IF EXISTS salud //
-CREATE FUNCTION salud(salario_base int) RETURNS int DETERMINISTIC
+CREATE FUNCTION salud(salario_base int) RETURNS float DETERMINISTIC
 BEGIN
-    DECLARE salud int;
+    DECLARE salud float;
     set salud = salario_base * 0.04;
     RETURN salud;
 END 
@@ -59,9 +59,9 @@ END
 --- Otra menera de hacer el ejercicio 2.
 DELIMITER //
 DROP FUNCTION IF EXISTS salud //
-CREATE FUNCTION salud(id int) RETURNS int DETERMINISTIC
+CREATE FUNCTION salud(id int) RETURNS float DETERMINISTIC
 BEGIN 
-    declare calcular_salud int;
+    declare calcular_salud float;
     set calcular_salud = (select salario_base * 0.04 from salario where identificador = id);
     RETURN calcular_salud;
 END 
@@ -70,9 +70,9 @@ END
 --- 3. Función pension: La pensión que corresponde al 4% al salario básico.
 DELIMITER //
 DROP FUNCTION IF EXISTS pension //
-CREATE FUNCTION pension(salario_base int) RETURNS int DETERMINISTIC
+CREATE FUNCTION pension(salario_base int) RETURNS float DETERMINISTIC
 BEGIN
-    DECLARE pension int;
+    DECLARE pension float;
     set pension = salario_base * 0.04;
     RETURN pension;
 END 
@@ -81,9 +81,9 @@ END
 --- Otra menra de hacer el ejercicio 3. 
 DELIMITER //
 DROP FUNCTION IF EXISTS pension //
-CREATE FUNCTION pension(id int) RETURNS int DETERMINISTIC
+CREATE FUNCTION pension(id int) RETURNS float DETERMINISTIC
 BEGIN 
-    declare calcular_pension int;
+    declare calcular_pension float;
     set calcular_pension = (select salario_base * 0.04 from salario where identificador = id);
     RETURN calcular_pension;
 END 
@@ -92,9 +92,9 @@ END
 --- 4. Función bono: Un bono que corresponde al 8% al salario básico.
 DELIMITER //
 DROP FUNCTION IF EXISTS bono //
-CREATE FUNCTION bono(salario_base int) RETURNS int DETERMINISTIC
+CREATE FUNCTION bono(salario_base int) RETURNS float DETERMINISTIC
 BEGIN
-    DECLARE bono int;
+    DECLARE bono float;
     set bono = salario_base * 0.08;
     RETURN bono;
 END 
@@ -103,9 +103,9 @@ END
 --- Otra menera de hacer el ejercicio 4. 
 DELIMITER //
 DROP FUNCTION IF EXISTS bono //
-CREATE FUNCTION bono(id int) RETURNS int DETERMINISTIC
+CREATE FUNCTION bono(id int) RETURNS float DETERMINISTIC
 BEGIN 
-    declare calcular_bono int;
+    declare calcular_bono float;
     set calcular_bono = (select salario_base * 0.08 from salario where identificador = id);
     RETURN calcular_bono;
 END 
@@ -114,18 +114,10 @@ END
 --- 5. Función integral: El salario integral es la suma del salario básico - salud - pension + bono + sub de transporte.
 DELIMITER //
 DROP FUNCTION IF EXISTS integral //
-CREATE FUNCTION integral(id int) RETURNS int DETERMINISTIC
+CREATE FUNCTION integral(id int) RETURNS float DETERMINISTIC
 BEGIN
     DECLARE calcular_integral int;
-    DECLARE funcion_salud int;
-    DECLARE funcion_pension int;
-    DECLARE funcion_bono int;
-    DECLARE funcion_subsidio_transporte int;
-    set funcion_salud = (select salud(id));
-    set funcion_pension = (select pension(id));
-    set funcion_bono = (select bono(id));
-    set funcion_subsidio_transporte = (select subsidio_transporte(id));
-    set calcular_integral = (select salario_base - funcion_salud - funcion_pension + funcion_bono + funcion_subsidio_transporte from salario where identificador = id);
+    set calcular_integral = (select salario_base - salud(id) - pension(id) + bono(id) + subsidio_transporte(id) from salario where identificador = id);
     RETURN calcular_integral;
 END 
 //
