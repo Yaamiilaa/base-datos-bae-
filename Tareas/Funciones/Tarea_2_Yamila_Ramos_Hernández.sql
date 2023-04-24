646314527d885c03677fbddad86b8480c5b32ef3
Yaamiilaa <113527458+Yaamiilaa@users.noreply.github.com>
2023-04-24 15:58:38 +0100

    Modificación de la tarea 2 de funciones

--- Tareas/Funciones/Tarea_2_Yamila_Ramos_Hernández.sql
+++ Tareas/Funciones/Tarea_2_Yamila_Ramos_Hernández.sql
@@ -6,15 +6,16 @@ mysql> use salario;
 Database changed
 
 --- Cración de la tabla
+drop table if EXISTS persona;
 create table persona(
     id varchar(9) not null primary key,
     nombre varchar(50),
     salario_base integer,
-    subsidio int,
-    salud int,
-    pension int,
-    bono int,
-    integral int
+    subsidio float,
+    salud float,
+    pension float,
+    bono float,
+    integral float
 );
 /* 
 mysql> create table persona(
@@ -30,7 +31,7 @@ mysql> create table persona(
 Query OK, 0 rows affected (0,02 sec)
  */
 
---- Procedimiento para registrar al emnos 10 personas.
+--- Procedimiento para registrar al menos 10 personas.
 DELIMITER //
 DROP PROCEDURE IF EXISTS insertar_personas //
 CREATE PROCEDURE insertar_personas(in numero_de_personas int)
@@ -62,28 +63,153 @@ mysql> CREATE PROCEDURE insertar_personas(in numero_de_personas int)
 Query OK, 0 rows affected (0,09 sec)
  */
 
+--- Función para calcular el porcentaje
+DELIMITER //
+DROP FUNCTION IF EXISTS calcular_porcentaje //
+CREATE FUNCTION calcular_porcentaje(numero int) RETURNS float DETERMINISTIC
+BEGIN 
+    declare calcular_porcentaje float;
+    set calcular_porcentaje = numero / 100;
+    RETURN calcular_porcentaje;
+end
+//
+/* 
+mysql> CREATE FUNCTION calcular_porcentaje(numero int) RETURNS float DETERMINISTIC
+    -> BEGIN 
+    ->     declare calcular_porcentaje float;
+    ->     set calcular_porcentaje = numero / 100;
+    ->     RETURN calcular_porcentaje;
+    -> end
+    -> //
+Query OK, 0 rows affected (0,02 sec) */
+
+
 --- Función de subsidio_transporte
 DELIMITER //
 DROP FUNCTION IF EXISTS subsidio_transporte //
-CREATE FUNCTION subsidio_transporte(identificador int) RETURNS FLOAT DETERMINISTIC
+CREATE FUNCTION subsidio_transporte(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE calcular_subsidio float;
-    SET calcular_subsidio = (select salario_base * 0.07 from persona where id = identificador);
-    update persona set subsidio = calcular_subsidio where id = identificador;  
-    RETURN calcular_subsidio;
+    DECLARE resultado float;
+    declare numero_porcentaje int;
+    set numero_porcentaje = 7;
+    SET resultado = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    update persona set subsidio = resultado where id = identificador;  
+    RETURN resultado;
 END 
 //
+/* 
+mysql> CREATE FUNCTION subsidio_transporte(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+    -> BEGIN
+    ->     DECLARE resultado float;
+    ->     declare numero_porcentaje int;
+    ->     set numero_porcentaje = 7;
+    ->     SET resultado = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    ->     update persona set subsidio = resultado where id = identificador;  
+    ->     RETURN resultado;
+    -> END 
+    -> //
+Query OK, 0 rows affected (0,00 sec)
+ */
 
 --- Función de salud.
 DELIMITER //
 DROP FUNCTION IF EXISTS modificar_salud //
-CREATE FUNCTION modificar_salud(identificador int) RETURNS FLOAT DETERMINISTIC
+CREATE FUNCTION modificar_salud(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
 BEGIN 
     DECLARE calcular_salud float;
-    SET calcular_salud = (select salario_base * 0.04 from persona where id = identificador);
-    update persona set salud = calcular_salud;
+    declare numero_porcentaje int;
+    set numero_porcentaje = 4;
+    SET calcular_salud = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    update persona set salud = calcular_salud where id = identificador;
     RETURN calcular_salud;
 END 
 //
+/* 
+mysql> CREATE FUNCTION modificar_salud(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+    -> BEGIN 
+    ->     DECLARE calcular_salud float;
+    ->     declare numero_porcentaje int;
+    ->     set numero_porcentaje = 4;
+    ->     SET calcular_salud = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    ->     update persona set salud = calcular_salud where id = identificador;
+    ->     RETURN calcular_salud;
+    -> END 
+    -> //
+Query OK, 0 rows affected (0,02 sec) */
 
 
+--- Función de pensión.
+DELIMITER //
+DROP FUNCTION IF EXISTS modificar_pension //
+CREATE FUNCTION modificar_pension(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+BEGIN 
+    DECLARE calcular_pension float;
+    declare numero_porcentaje int;
+    set numero_porcentaje = 4;
+    SET calcular_pension = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    update persona set pension = calcular_pension where id = identificador;
+    RETURN calcular_pension;
+END 
+//
+/* 
+mysql> CREATE FUNCTION modificar_pension(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+    -> BEGIN 
+    ->     DECLARE calcular_pension float;
+    ->     declare numero_porcentaje int;
+    ->     set numero_porcentaje = 4;
+    ->     SET calcular_pension = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    ->     update persona set pension = calcular_pension where id = identificador;
+    ->     RETURN calcular_pension;
+    -> END 
+    -> //
+Query OK, 0 rows affected (0,02 sec)
+
+ */
+
+--- Función bono.
+DELIMITER //
+DROP FUNCTION IF EXISTS modificar_bono //
+CREATE FUNCTION modificar_bono(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+BEGIN 
+    DECLARE calcular_bono float;
+    declare numero_porcentaje int;
+    set numero_porcentaje = 8;
+    SET calcular_bono = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    update persona set bono = calcular_bono where id = identificador;
+    RETURN calcular_bono;
+END 
+//
+/* 
+mysql> CREATE FUNCTION modificar_bono(identificador varchar(9)) RETURNS FLOAT DETERMINISTIC
+    -> BEGIN 
+    ->     DECLARE calcular_bono float;
+    ->     declare numero_porcentaje int;
+    ->     set numero_porcentaje = 8;
+    ->     SET calcular_bono = (select salario_base * calcular_porcentaje(numero_porcentaje) from persona where id = identificador);
+    ->     update persona set bono = calcular_bono where id = identificador;
+    ->     RETURN calcular_bono;
+    -> END 
+    -> //
+Query OK, 0 rows affected (0,00 sec) */
+
+--- Función integral.
+DELIMITER //
+DROP FUNCTION IF EXISTS modificar_integral //
+CREATE FUNCTION modificar_integral(identificador varchar(9)) RETURNS float DETERMINISTIC
+BEGIN
+    DECLARE calcular_integral float;
+    DECLARE calcular_salud FLOAT;
+    DECLARE salario FLOAT;
+    declare calcular_pension float;
+    DECLARE calcular_bono float;
+    declare subsidio_transporte float;
+    set calcular_bono = modificar_bono(identificador);
+    set calcular_pension = modificar_pension(identificador);
+    set subsidio_transporte = subsidio_transporte(identificador);
+    set calcular_salud = modificar_salud(identificador);
+    set salario = (select salario_base from persona where id = identificador); 
+    set calcular_integral = (salario - calcular_salud - calcular_pension + calcular_bono + subsidio_transporte);
+    update persona set integral = calcular_integral where id = identificador;
+    RETURN calcular_integral;
+END 
+//
